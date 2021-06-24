a9d873600b652c5502c79ae85023281080f1951a
Victor de Paula <victorssspaula@live.com>
2021-06-24 15:11:42 -0300

    exercicio 2 js dia 2

--- JS-dia-2/script.js
+++ JS-dia-2/script.js
@@ -1,4 +1,4 @@
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu[1];
+let indexOfPortfolio = menu.indexOf('Portfólio');
 
-console.log(menuServices);
+console.log(indexOfPortfolio);
