c4da29a9e50f9d37833b2076400661af14fb1144
Victor de Paula <victorssspaula@live.com>
2021-06-24 18:39:01 -0300

    exercicio 2 JS dia 2

--- exercises/introducao-a-javascript-e-logica-de-programacao.2/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao.2/script.js
@@ -1,5 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0
 
-for (let percorrer of numbers) {
-  console.log(percorrer);
+for (let index = 0; index < numbers.length; index += 1) {
+  resultado += numbers[index]
 }
+console.log(resultado)
