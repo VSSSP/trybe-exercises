5f819daf3d34eb717366616bbcaada03110e89e1
Victor de Paula <victorssspaula@live.com>
2021-06-24 11:24:50 -0300

    exercicio 2 javascript dia 1

--- js-exercises/script.js
+++ js-exercises/script.js
@@ -1,8 +1,8 @@
 let a = 15
 let b = 37
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
+if (a > b) {
+  console.log(a)
+}else{
+  console.log(b)
+}
