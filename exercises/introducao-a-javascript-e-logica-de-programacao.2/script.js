292720b3d5c925f5fc1f3ebb03fca2f8bcb730dd
Victor de Paula <victorssspaula@live.com>
2021-06-24 19:15:07 -0300

    exercicio 7 JS dia 2

--- exercises/introducao-a-javascript-e-logica-de-programacao.2/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao.2/script.js
@@ -1,15 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let higherNumber = 0;
+let higherNumber = numbers[0];
 
 for (let index = 0; index < numbers.length; index += 1) {
-  if (numbers[index] % 2 === 0) {
-    higherNumber += 1;
+  if (numbers[index] <= higherNumber) {
+    higherNumber = numbers[index];
   }
 }
 
 console.log (higherNumber)
-
-if (higherNumber === 0) {
-  console.log('nenhum valor impar encontrado')
-}
