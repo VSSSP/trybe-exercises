eacb89ed832fc6168d77f2bacc5a40152d934124
Victor de Paula <victorssspaula@live.com>
2021-06-25 15:42:54 -0300

    Exercicio 1 JS dia 4

---
+++ logica-js/logica.js
@@ -0,0 +1,10 @@
+let n = 5
+let asteriscos = '*'
+let espaco = ''
+
+for (let index = 0; index < n; index += 1) {
+  espaco = espaco + asteriscos
+}
+for (let index = 0; index < n; index += 1) {
+  console.log (espaco)
+}
