9f5f013a35710f51b237bae24eb0914e829d2dcf
Victor de Paula <victorssspaula@live.com>
2021-06-28 15:30:12 -0300

    marta 3

--- fixacao/fix.js
+++ fixacao/fix.js
@@ -1,16 +1,19 @@
-let player = {
-  name: 'Marta',
-  lastName: 'Silva',
-  age: 34,
-  medals: {
-    golden: 2,
-    silver: 3,
-  } ,
-  bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+};
+
+for (let key in names) {
+  console.log ('Olá ' + names[key])
 }
 
-console.log ('A jogadora' + ' ' + player.name + ' ' + player.lastName + ' ' + 'tem' + ' ' + player.age + ' ' + 'anos de idade'  )
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
 
-console.log ('A jogadora' + ' ' + player.name + ' ' + player.lastName + ' ' + 'foi eleita a melhor do mundo por' + ' ' + player.bestInTheWorld.length + ' ' + 'vezes.')
-
-console.log ('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata' )
+for (let key in car) {
+  console.log (key + ': ' + car[key])
+}
