a1c5b0c21f3b358f41ccff55c368c4658030165f
Victor de Paula <victorssspaula@live.com>
2021-06-28 19:39:57 -0300

    exercicios finais de for/in

--- fixacao/exercicios.js
+++ fixacao/exercicios.js
@@ -4,12 +4,25 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-console.log ('Bem vinda,', info.personagem)
+console.log('Bem vinda,', info.personagem)
 
 info.recorrente = 'Sim'
 
-console.log (info)
+console.log(info)
+
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dells Four color Comics #178',
+  nota: 'O ultimo MacPatinhas',
+  recorrente: 'Sim'
+};
+
 
 for (let key in info) {
-  console.log(info[key])
-}
+    if (key === 'recorrente' && info.recorrente === 'Sim' && info2.recorrente === 'Sim') {
+      console.log ('Ambas Recorrentes')
+    } else {
+      console.log (info[key], 'e', info2[key])
+    }
+  }
