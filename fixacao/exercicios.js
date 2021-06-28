466ff753402074ea75dbd5cb5a11c2c111a56c21
Victor de Paula <victorssspaula@live.com>
2021-06-28 19:14:44 -0300

    exercicios 1, 2 e 3

---
+++ fixacao/exercicios.js
@@ -0,0 +1,15 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+console.log ('Bem vinda,', info.personagem)
+
+info.recorrente = 'Sim'
+
+console.log (info)
+
+for (let key in info) {
+  console.log(info[key])
+}
