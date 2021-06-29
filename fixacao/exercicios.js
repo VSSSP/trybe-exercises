a961669653588441cc8e4d905404ed7b64968056
Victor de Paula <victorssspaula@live.com>
2021-06-28 21:39:09 -0300

    exercicio 4  JS 4.4

--- fixacao/exercicios.js
+++ fixacao/exercicios.js
@@ -21,26 +21,38 @@
 // console.log (verificaPalindromo('arara'))
 // console.log (verificaPalindromo2('desenvolvimento'))
 
-function arrayInteiros (array) {
-  let armazena = 0
-  for (let key in array) {
-    if (armazena < array[key]) {
-      armazena = key
+// function arrayInteiros (array) {
+//   let armazena = 0
+//   for (let key in array) {
+//     if (armazena < array[key]) {
+//       armazena = key
+//     }
+//   }
+//   return armazena
+// }
+
+// console.log (arrayInteiros([2, 3, 6, 7, 10, 1]))
+
+// function menorNumero (menor) {
+//   let armazene = 1
+//   for (let key in menor) {
+//     if (armazene > menor[key]) {
+//       armazene = key
+//     }
+//   }
+//   return armazene
+// }
+
+// console.log(menorNumero([2, 4, 6, 7, 10, 0, -3]))
+
+function nomeGrande (nome) {
+  let maiorNome = 'a'
+  for (let key in nome) {
+    if (nome.length > maiorNome.length) {
+      maiorNome = nome[key]
     }
   }
-  return armazena
+return maiorNome
 }
 
-console.log (arrayInteiros([2, 3, 6, 7, 10, 1]))
-
-function menorNumero (menor) {
-  let armazene = 1
-  for (let key in menor) {
-    if (armazene > menor[key]) {
-      armazene = key
-    }
-  }
-  return armazene
-}
-
-console.log(menorNumero([2, 4, 6, 7, 10, 0, -3]))
+console.log (nomeGrande(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']))
