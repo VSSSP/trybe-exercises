b0075ded0635db79cb5025ad343749cd54afc10d
Victor de Paula <victorssspaula@live.com>
2021-06-28 21:21:39 -0300

    exercicio 1 JS 4.4

--- fixacao/exercicios.js
+++ fixacao/exercicios.js
@@ -1,28 +1,22 @@
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-};
-
-console.log('Bem vinda,', info.personagem)
-
-info.recorrente = 'Sim'
-
-console.log(info)
 
 
-let info2 = {
-  personagem: 'Tio Patinhas',
-  origem: 'Christmas on Bear Mountain, Dells Four color Comics #178',
-  nota: 'O ultimo MacPatinhas',
-  recorrente: 'Sim'
-};
-
-
-for (let key in info) {
-    if (key === 'recorrente' && info.recorrente === 'Sim' && info2.recorrente === 'Sim') {
-      console.log ('Ambas Recorrentes')
-    } else {
-      console.log (info[key], 'e', info2[key])
-    }
+function verificaPalindromo (palavra) {
+  let palindromo = palavra.split('').reverse().join('');
+  if (palavra === palindromo) {
+    return true
+  } else {
+    return false
   }
+}
+
+function verificaPalindromo2 (outraPalavra) {
+  let palindromo = outraPalavra.split('').reverse().join('');
+  if (outraPalavra === palindromo) {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log (verificaPalindromo('arara'))
+console.log (verificaPalindromo2('desenvolvimento'))
