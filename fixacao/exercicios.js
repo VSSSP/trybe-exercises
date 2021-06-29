909ecd8d43f6bfd861658341b42abce503fbede7
Victor de Paula <victorssspaula@live.com>
2021-06-28 21:34:50 -0300

    exercicio 2 e 3 JS 4.4

--- fixacao/exercicios.js
+++ fixacao/exercicios.js
@@ -1,22 +1,46 @@
 
 
-function verificaPalindromo (palavra) {
-  let palindromo = palavra.split('').reverse().join('');
-  if (palavra === palindromo) {
-    return true
-  } else {
-    return false
+// function verificaPalindromo (palavra) {
+//   let palindromo = palavra.split('').reverse().join('');
+//   if (palavra === palindromo) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+// function verificaPalindromo2 (outraPalavra) {
+//   let palindromo = outraPalavra.split('').reverse().join('');
+//   if (outraPalavra === palindromo) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+
+// console.log (verificaPalindromo('arara'))
+// console.log (verificaPalindromo2('desenvolvimento'))
+
+function arrayInteiros (array) {
+  let armazena = 0
+  for (let key in array) {
+    if (armazena < array[key]) {
+      armazena = key
+    }
   }
+  return armazena
 }
 
-function verificaPalindromo2 (outraPalavra) {
-  let palindromo = outraPalavra.split('').reverse().join('');
-  if (outraPalavra === palindromo) {
-    return true
-  } else {
-    return false
+console.log (arrayInteiros([2, 3, 6, 7, 10, 1]))
+
+function menorNumero (menor) {
+  let armazene = 1
+  for (let key in menor) {
+    if (armazene > menor[key]) {
+      armazene = key
+    }
   }
+  return armazene
 }
 
-console.log (verificaPalindromo('arara'))
-console.log (verificaPalindromo2('desenvolvimento'))
+console.log(menorNumero([2, 4, 6, 7, 10, 0, -3]))
