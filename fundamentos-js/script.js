d5c026c40438555f9a6b2556eedcbd3810050721
Victor de Paula <victorssspaula@live.com>
2021-06-30 19:04:32 -0300

    Exercicios prontos, Fundamentos JS dia 1

--- fundamentos-js/script.js
+++ fundamentos-js/script.js
@@ -1,7 +1,38 @@
-document.getElementsByTagName('p')[1].innerText = 'Me vejo trabalhando em programação'
+function mudaTexto () {
+let textoNovo = document.getElementsByTagName('p')[1]
+textoNovo.innerText = 'Me vejo trabalhando em programação'
+}
+mudaTexto()
 
 function mudarCor() {
-let cor = document.getElementsByClassName('main-content')[0];
-cor.style.background = "rgb(76,164,109)";
+  let cor = document.getElementsByClassName('main-content')[0];
+  cor.style.background = "rgb(76,164,109)";
 }
 mudarCor();
+
+function corzeta() {
+  let cor = document.getElementsByClassName('center-content')[0]
+  cor.style.background = 'white'
+}
+corzeta();
+
+function corrigeTexto() {
+  let texto = document.getElementsByTagName('h1')[0]
+  texto.innerText = 'Exercicio 5.1 - JavaScript';
+}
+corrigeTexto();
+
+function upCase () {
+  let up = document.getElementsByTagName('p')[1]
+  up.innerText = up.innerText.toUpperCase ()
+}
+upCase ()
+
+function exibeTudo () {
+  let exibe = document.getElementsByTagName('p')
+  for (let key of exibe) {
+    console.log (key.innerText)
+  }
+}
+
+exibeTudo ()
