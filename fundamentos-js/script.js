ef66b3ece10080b99581d797631768754cdddbb5
Victor de Paula <victorssspaula@live.com>
2021-06-30 18:45:09 -0300

    Exercicios 1 e 2, fundamentos JS dia 1

---
+++ fundamentos-js/script.js
@@ -0,0 +1,7 @@
+document.getElementsByTagName('p')[1].innerText = 'Me vejo trabalhando em programação'
+
+function mudarCor() {
+let cor = document.getElementsByClassName('main-content')[0];
+cor.style.background = "rgb(76,164,109)";
+}
+mudarCor();
