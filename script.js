64a90802b808e1316f93190b7d891d8a88300485
Victor de Paula <victorssspaula@live.com>
2021-07-05 18:26:14 -0300

    Exercicios 1 e 2

--- script.js
+++ script.js
@@ -6,22 +6,39 @@
 // console.log(irmaoElementoOndeVoceEsta)
 
 
-// irmão pro 'elemento onde vc esta'
-const pai = document.getElementById('pai');
-const irmaoElementoOndeVoceEsta = document.createElement('div');
-irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
-pai.appendChild(irmaoElementoOndeVoceEsta)
-// console.log(irmaoElementoOndeVoceEsta)
-;
-// filho do elemento onde vc esta
-const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta')
-const filhoDoElemento = document.createElement('div')
-filhoDoElemento.id = 'filhoDoElemento'
-elementoOndeVoceEsta.appendChild(filhoDoElemento)
-// console.log (filhoDoElemento)
+// // irmão pro 'elemento onde vc esta'
+// const pai = document.getElementById('pai');
+// const irmaoElementoOndeVoceEsta = document.createElement('div');
+// irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
+// pai.appendChild(irmaoElementoOndeVoceEsta)
+// // console.log(irmaoElementoOndeVoceEsta)
+// ;
+// // filho do elemento onde vc esta
+// const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta')
+// const filhoDoElemento = document.createElement('div')
+// filhoDoElemento.id = 'filhoDoElemento'
+// elementoOndeVoceEsta.appendChild(filhoDoElemento)
+// // console.log (filhoDoElemento)
 
-const primeiroFilhodoFilho = document.getElementById('primeiroFilhodoFilho')
-const filhoMaisNovo = document.createElement('div')
-filhoMaisNovo.id = 'filhoMaisNovo'
-primeiroFilhodoFilho.appendChild(filhoMaisNovo)
+// const primeiroFilhodoFilho = document.getElementById('primeiroFilhodoFilho')
+// const filhoMaisNovo = document.createElement('div')
+// filhoMaisNovo.id = 'filhoMaisNovo'
+// primeiroFilhodoFilho.appendChild(filhoMaisNovo)
 
+// const body = document.querySelector('body')
+
+// const element = document.createElement('h1')
+
+// body.appendChild (element)
+
+// document.querySelector('h1')[0].innerText = 'Exercício 5.2 - JavaScript'
+
+const element = document.createElement('h1');
+element.innerHTML = 'Exercício 5.2 - JavaScript DOM'; 
+document.body.appendChild(element)
+
+const element1 = document.createElement ('div') ;
+
+element1.className = 'main-content'
+
+document.body.appendChild(element1)
