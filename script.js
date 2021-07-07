55f0a1f1793983573e95e08e40379e87fe5645df
Victor de Paula <victorssspaula@live.com>
2021-07-07 18:36:23 -0300

    Iniciando os Exercicios

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
-function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-  const weekDaysList = document.querySelector('.week-days');
+const button = document.getElementById('add-button');
+const input = document.getElementById('phrases-input');
+const list = document.getElementById('phrases-list');
 
-  for (let index = 0; index < weekDays.length; index += 1) {
-    const days = weekDays[index];
-    const dayListItem = document.createElement('li');
-    dayListItem.innerHTML = days;
-
-    weekDaysList.appendChild(dayListItem);
-  };
+function addPhraseToSessionStorage() {
+  if (sessionStorage.getItem('phrases') === null) {
+    sessionStorage.setItem('phrases', JSON.stringify([]));
+  }
+  const oldList = JSON.parse(sessionStorage.getItem('phrases'));
+  const phraseText = input.value;
+  oldList.push(phraseText);
+  sessionStorage.setItem('phrases', JSON.stringify(oldList));
+  insertPhraseInDOM();
 };
 
-createDaysOfTheWeek();
+function insertPhraseInDOM() {
+  const phrasesList = JSON.parse(sessionStorage.getItem('phrases'));
+  const listLength = phrasesList.length - 1;
+  const phraseText = phrasesList[listLength];
+  const phrase = document.createElement('li');
+  phrase.innerText = phraseText;
+  list.appendChild(phrase);
+};
 
-// Escreva seu código abaixo.
-
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-
-const dias = document.querySelector('#days')
-
-for (let i = 0; i < dezDaysList.length; i += 1) {
-  let diasDoMes = dezDaysList[i]
-  let osDiasDoMes = document.createElement('li');
-  osDiasDoMes.innerText = diasDoMes
-  osDiasDoMes.className = 'day'
-  if (dezDaysList[i] === 24 || dezDaysList[i] === 25 || dezDaysList[i] === 31) {
-    osDiasDoMes.className = 'holiday'
-  } 
-  else if (dezDaysList[i] === 4 || dezDaysList[i] === 11 || dezDaysList[i] === 18 || dezDaysList[i] === 25) {
-    osDiasDoMes.className = 'friday'
-  }
-
-  dias.appendChild(osDiasDoMes);
-}
+button.addEventListener('click', addPhraseToSessionStorage);
