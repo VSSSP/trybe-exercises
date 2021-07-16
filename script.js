a7be104b8804a46ef816480d673937bf672be26f
Victor de Paula <victorssspaula@live.com>
2021-07-16 13:53:05 -0300

    Data

---
+++ script.js
@@ -0,0 +1,18 @@
+const todosEstados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+const selectEstados = document.getElementById('estados')
+const data = document.getElementById('data')
+
+todosEstados.forEach(element => {
+  const options = document.createElement('option')
+  options.innerHTML = element
+  selectEstados.appendChild(options)
+});
+
+function dataAtualFormatada(){
+      dia  = data.getDate().toString(),
+      diaF = (dia.length == 1) ? '0'+dia : dia,
+      mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+      mesF = (mes.length == 1) ? '0'+mes : mes,
+      anoF = data.getFullYear();
+  return diaF+"/"+mesF+"/"+anoF;
+}
