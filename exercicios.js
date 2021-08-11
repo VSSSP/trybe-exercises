9ca6688181289dfa1cd0ffad904139fb39931991
VSSSP <victorssspaula@live.com>
2021-08-11 11:46:36 -0300

    Exercicio 3

--- exercicios.js
+++ exercicios.js
@@ -1,3 +1,5 @@
+// Exercicio 1
+
 // const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
 //   `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
@@ -30,6 +32,8 @@
 // console.log(planetDistanceFromSun(jupiter)); // C
 // correto
 
+// Exercicio 2
+
 // const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
 //   `${name} is ${value} ${measurementUnit} apart from the Sun`;
 
@@ -60,3 +64,20 @@
 // console.log(planetDistanceFromSun(mars)); // A
 // setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // C
 // setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // B
+
+// Exercicio 3
+
+const getPlanet = () => {
+    setTimeout(() => {
+    const mars = {
+      name: "Mars",
+      distanceFromSun: {
+        value: 227900000,
+        measurementUnit: "kilometers",
+      },
+    };
+    console.log("Returned planet: ", mars);
+}, 4000)
+  };
+  
+  getPlanet(); // imprime Marte depois de 4 segundos
