cb9c4ac0ef9f16fff774fa9cadcba7b2b71f9118
VSSSP <victorssspaula@live.com>
2021-08-11 11:59:22 -0300

    Exercicio 4

--- exercicios.js
+++ exercicios.js
@@ -81,3 +81,21 @@ const getPlanet = () => {
   };
   
   getPlanet(); // imprime Marte depois de 4 segundos
+
+// Exercicio 4
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  
+    const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+
+const temperature = (callback, time) => {
+    setTimeout(() => console.log(`Mars temperature is: ${callback} degree Celsius`), time);
+}; // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+temperature(getMarsTemperature(), messageDelay());
