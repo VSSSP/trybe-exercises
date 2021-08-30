fdbc1bb3eb757c83633620c1e038ede1c8b38429
VSSSP <victorssspaula@live.com>
2021-08-30 19:49:46 -0300

    appReactClass

--- new/src/App.js
+++ new/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+const Task = (value) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li key={value}>{value}</li>
   );
+}
+
+const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
+
+class App extends Component {
+  render() {
+    return (
+      <ul>{ tarefas.map((tarefa) => Task(tarefa)) }</ul>
+    )
+  }
 }
 
 export default App;
